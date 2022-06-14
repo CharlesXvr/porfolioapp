5533a83fce4ab5b2962c5f0c63c97a9977be008c
CharlesXvr <thestuntman92@gmail.com>
2022-06-14 14:09:53 -0300

    pages added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'', redirectTo:'home', pathMatch:'full'},
+  { path: 'login', loadChildren: () => import('./modules/pages/auth/login/login.module').then(m => m.LoginModule) },
+  { path: 'home', loadChildren: () => import('./modules/pages/home/home.module').then(m => m.HomeModule) },
+  { path: 'portfolio', loadChildren: () => import('./modules/pages/portfolio/portfolio.module').then(m => m.PortfolioModule) },
+  { path: '**', loadChildren: () => import('./modules/pages/not-found/not-found.module').then(m => m.NotFoundModule) }, 
+
+
+];
+
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,18 +7,25 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './shared/components/header/header.component';
 import { FooterComponent } from './shared/components/footer/footer.component';
+import { FormsModule } from '@angular/forms';
+import { PortfolioModule } from './modules/pages/portfolio/portfolio.module';
+import { PortfolioComponent } from './modules/pages/portfolio/portfolio.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    PortfolioComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     MDBBootstrapModule.forRoot(),
-    MdbCheckboxModule
+    MdbCheckboxModule,
+    FormsModule,
+    PortfolioModule
   ],
   providers: [],
   bootstrap: [AppComponent]
